f1b649a1f615d073075b32962aa6d27cb314b78f
Jamie Harding <jamiegharding@gmail.com>
2025-05-14 17:10:51 -0400

    Created app python server

--- run_python_server.bat
+++ run_python_server.bat
@@ -1,3 +1,7 @@
-start chrome.exe http://localhost:8000/
 @echo off
-python -m http.server 8000
+:: Start the HTTP server minimized with logging
+start /min cmd /c "python -m http.server 8000"
+
+:: Open the web app in its own app window
+start chrome.exe --app=http://localhost:8000/
+exit
